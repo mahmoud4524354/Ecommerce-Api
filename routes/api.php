a4b7cf2bc3052dfd22d91ab89ae57bd2d064c8e9
Mahmoud_Magdy <mahmoud30204050@gmail.com>
2025-10-26 16:47:48 +0300

    Add auth system with Sanctum

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+    Route::get('/user', 'user')->middleware('auth:sanctum');
+});
